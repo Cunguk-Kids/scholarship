eece99aa7b7dfee547ff3bf2a457783387e0eca6
RaihanArdianata <raihan.ardianata@gmail.com>
2025-07-27 22:41:05 +0700

    update db config

--- packages/ponder/src/db/index.ts
+++ packages/ponder/src/db/index.ts
@@ -5,14 +5,14 @@ import * as schema from "./schema";
 import { ReadonlyDrizzle } from "ponder";
 
 const pool = new Pool({
-  host: process.env.DB_HOST!,
-  user: process.env.DB_USER!,
-  password: process.env.DB_PASSWORD!,
-  database: process.env.DB_NAME!,
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
   port: 5432,
   ssl: {
     rejectUnauthorized: false
-  },
+  }
   // connectionString: process.env.DATABASE_URL!,
 });
 
