126766a29914ba62f4179c10df9a06ab43f1556d
RaihanArdianata <raihan.ardianata@gmail.com>
2025-07-27 21:53:03 +0700

    using native

--- packages/ponder/src/db/index.ts
+++ packages/ponder/src/db/index.ts
@@ -5,10 +5,15 @@ import * as schema from "./schema";
 import { ReadonlyDrizzle } from "ponder";
 
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL!,
+  host: 'ep-purple-pond-admrht0o-pooler.c-2.us-east-1.aws.neon.tech',
+  user: 'neondb_owner',
+  password: 'npg_J9NPIGmL6BSd',
+  database: 'neondb',
+  port: 5432,
   ssl: {
     rejectUnauthorized: false
-  },
+  }
+  // connectionString: process.env.DATABASE_URL!,
 });
 
 export const db = drizzle(pool, { schema });
