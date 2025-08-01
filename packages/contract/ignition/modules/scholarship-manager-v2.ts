764755689096fcee793d02cbed6e052d400b1386
Adzikri Fauzi Shiddiq <39423103+Rakemoon@users.noreply.github.com>
2025-08-01 14:04:11 +0700

    chore: contract fix cannot mint nft (#125)

--- packages/contract/ignition/modules/scholarship-manager-v2.ts
+++ packages/contract/ignition/modules/scholarship-manager-v2.ts
@@ -2,14 +2,8 @@ import { buildModule } from "@nomicfoundation/hardhat-ignition/modules";
 import { keccak256, toHex } from "viem";
 
 export default buildModule("ScholarshipManagerModuleV2", (m) => {
-  const donaterNFT = m.contractAt(
-    "ProgramCreatorNFT",
-    "0x6cB6d54108DfB4d8Dce2D8fAdbfe8465f222d3B5"
-  );
-  const studentNFT = m.contractAt(
-    "ApplicantNFT",
-    "0x282170e69DE842a323911c37716e28A878c1899F"
-  );
+  const donaterNFT = m.contract("ProgramCreatorNFT");
+  const studentNFT = m.contract("ApplicantNFT");
   const usdc = m.contractAt(
     "MockUSDC",
     "0x9b379eA3B4dEE91E1B0F2e5c36C0931cCDf227a0"
--- packages/frontend/src/constants/contractAddress.ts
+++ packages/frontend/src/constants/contractAddress.ts
@@ -1,5 +1,5 @@
 export const skoolchainAddress = import.meta.env.VITE_CONTRACT_ADDRESS as `0x0`;
 export const skoolchainV2Address =
-  "0x42D68F24469b681d86A1d62E54D256b13718567a" as const;
+  "0xA5aC7aA1d96B7Aa58a565F8eAAC89575ddD19eb7" as const;
 export const usdcAddress =
   "0x9b379eA3B4dEE91E1B0F2e5c36C0931cCDf227a0" as const;
