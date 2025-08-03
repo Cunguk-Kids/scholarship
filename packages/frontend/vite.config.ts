346c6e3126441c118050dec7bddf30903dd07d4e
Rakemoon <rakemoonkuzmin@gmail.com>
2025-08-03 14:20:48 +0700

    chore: remove it from development

--- packages/frontend/vite.config.ts
+++ packages/frontend/vite.config.ts
@@ -3,7 +3,6 @@ import react from "@vitejs/plugin-react-swc";
 import tailwindcss from "@tailwindcss/vite";
 import { tanstackRouter } from "@tanstack/router-plugin/vite";
 import path from "node:path";
-import netlify from "@netlify/vite-plugin";
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -12,7 +11,6 @@ export default defineConfig({
       target: "react",
       autoCodeSplitting: true,
     }),
-    netlify(),
     react(),
     tailwindcss(),
   ],
