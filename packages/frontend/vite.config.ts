4b16ced8e2bbfef310d173be4211be4573fea7a2
Rakemoon <rakemoonkuzmin@gmail.com>
2025-08-03 14:09:16 +0700

    chore: add netlify conf

--- packages/frontend/vite.config.ts
+++ packages/frontend/vite.config.ts
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react-swc";
 import tailwindcss from "@tailwindcss/vite";
 import { tanstackRouter } from "@tanstack/router-plugin/vite";
 import path from "node:path";
+import netlify from "@netlify/vite-plugin";
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -11,6 +12,7 @@ export default defineConfig({
       target: "react",
       autoCodeSplitting: true,
     }),
+    netlify(),
     react(),
     tailwindcss(),
   ],
