4297eb00bea5f3e926d94ce3f76f3238c4b2533c
RaihanArdianata <raihan.ardianata@gmail.com>
2025-08-03 16:11:20 +0700

    update schema

---
+++ packages/ponder/drizzle/0005_classy_the_twelve.sql
@@ -0,0 +1,1 @@
+ALTER TABLE "milestones" ALTER COLUMN "amount" SET DATA TYPE double precision;
---
+++ packages/ponder/drizzle/0006_careful_rhino.sql
@@ -0,0 +1,2 @@
+ALTER TABLE "milestones" ALTER COLUMN "amount" SET DATA TYPE numeric;--> statement-breakpoint
+ALTER TABLE "milestones" ALTER COLUMN "amount" SET DEFAULT '0';
