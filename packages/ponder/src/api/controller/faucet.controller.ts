6c34b41d0f59556bb00fcad8b375e19dfac3d809
Rakemoon <rakemoonkuzmin@gmail.com>
2025-08-18 21:58:10 +0700

    call transaction on faucet controller

--- packages/ponder/src/api/controller/faucet.controller.ts
+++ packages/ponder/src/api/controller/faucet.controller.ts
@@ -4,6 +4,9 @@ import { faucetData, programs, students, votes } from "@/db/schema";
 import { and, eq, gt } from "drizzle-orm";
 import { getClientIP } from "@/utils/getClientIP";
 import { faucetSchemaType } from "../validators/faucet.validation";
+import { contractAddress, walletClient } from "../constants/config";
+import { scholarshipAbi } from "abis/abi";
+import { parseEther } from "viem";
 
 export const faucetController = async (c: Context) => {
   const body = await c.req.parseBody();
@@ -11,7 +14,7 @@ export const faucetController = async (c: Context) => {
 
   let ipAddress = getClientIP(c);
 
-  const req = c.req.raw as Request & { socket?: any; };
+  const req = c.req.raw as Request & { socket?: any };
   const socket = req?.socket;
   if (socket?.remoteAddress) ipAddress = socket.remoteAddress;
 
@@ -21,39 +24,49 @@ export const faucetController = async (c: Context) => {
   const [faucet] = await db
     .select()
     .from(faucetData)
-    .where(and(
-      eq(faucetData.address, address),
-      eq(faucetData.ipAddress, ipAddress),
-    ));
+    .where(
+      and(eq(faucetData.address, address), eq(faucetData.ipAddress, ipAddress))
+    );
 
   if (faucet) {
     if (faucet?.updatedAt && faucet?.updatedAt < oneDayAgo) {
       return c.json(
         {
           message: "you can only claim once every 24 hours",
-          nextClaimAt: new Date(faucet.updatedAt.getTime() + 24 * 60 * 60 * 1000),
+          nextClaimAt: new Date(
+            faucet.updatedAt.getTime() + 24 * 60 * 60 * 1000
+          ),
         },
         400
       );
     }
   }
 
-  await db.insert(faucetData).values({
-    address: String(address),
-    ipAddress: ipAddress || ''
-  }).onConflictDoUpdate({
-    target: [faucetData.address, faucetData.ipAddress],
-    set: {
-      address,
-      ipAddress,
-      updatedAt: new Date(),
-    }
+  await db
+    .insert(faucetData)
+    .values({
+      address: String(address),
+      ipAddress: ipAddress || "",
+    })
+    .onConflictDoUpdate({
+      target: [faucetData.address, faucetData.ipAddress],
+      set: {
+        address,
+        ipAddress,
+        updatedAt: new Date(),
+      },
+    });
+
+  const txHash = await walletClient.sendTransaction({
+    account: contractAddress,
+    to: address as `0x0`,
+    value: parseEther("0.01"),
   });
 
   return c.json({
     data: {
       address,
-    }
+      txHash,
+    },
   });
-
 };
