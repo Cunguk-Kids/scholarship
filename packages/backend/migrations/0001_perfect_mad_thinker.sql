ee5969e004a930c39afde8bc6084ce283f7ca77a
Rakemoon <rakemoonkuzmin@gmail.com>
2025-08-26 13:47:01 +0700

    chore: bersih bersih 2

--- packages/backend/migrations/0001_perfect_mad_thinker.sql
+++
@@ -1,4 +0,0 @@
-ALTER TABLE "applicants" ADD COLUMN "applicant_address" varchar NOT NULL;--> statement-breakpoint
-ALTER TABLE "milestone" ADD COLUMN "title" varchar NOT NULL;--> statement-breakpoint
-ALTER TABLE "milestone" ADD COLUMN "prove_metadata" varchar;--> statement-breakpoint
-ALTER TABLE "milestone" ADD COLUMN "prove_review" varchar;
--- packages/backend/src/utils/generator.ts
+++
@@ -1,7 +0,0 @@
-import crypto from "crypto";
-
-export function generateProgramId(contractAddress: string, timestamp = new Date().toISOString()) {
-  const raw = `${contractAddress}-${timestamp}`;
-  const hash = crypto.createHash("sha256").update(raw).digest("hex").slice(0, 16);
-  return { id: hash, timestamp };
-}
